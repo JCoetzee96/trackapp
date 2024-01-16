f07744cac5eafb7b4c6260baaf02ff78a79870ac
jankecoetzee <jankecoetzee@hotmail.com>
2024-01-16 18:35:47 +0100

    test

--- app.py
+++ app.py
@@ -1,52 +1,51 @@
 import streamlit as st
-# import streamlit_authenticator as stauth
-import datetime
-import re
-from deta import Deta
+import streamlit_authenticator as stauth
+from dependencies import sign_up, fetch_users
 
-deta_key = "a0hkj1bnvev_7P3cA3n5tmGqFMu4zrqEHKrah9su9hJQ"
-deta = Deta(deta_key)
-db = deta.Base('Authentication')
+st.set_page_config(page_title='Your personalised tracking app', page_icon='👾', initial_sidebar_state='collapsed',)
 
-def insert_user(email, username, password):
-    """Insert users to DB"""
-    date_joined = str(datetime.datetime.now())
-    return db.put({'key':email, 'username':username, 'password': password, 'date_joined':date_joined})
+try:
+    users = fetch_users()
+    emails = []
+    usernames = []
+    passwords = []
 
-def fetch_users():
-    """Fetch users"""
-    return db.fetch().items
+    for user in users:
+        emails.append(user['key'])
+        usernames.append(user['username'])
+        passwords.append(user['password'])
 
-def get_user_emails():
-    """Fetch user emails"""
-    return [user['key'] for user in db.fetch().items]
+    credentials = {'usernames': {}}
+    for index in range(len(emails)):
+        credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
 
-def get_usernames():
-    """"Fetch usernames"""
-    return [user['key'] for user in db.fetch().items]
+    Authenticator = stauth.Authenticate(credentials, cookie_name='OHVFSWBG', key='ecc992ba23ba5c3e', cookie_expiry_days=0)
 
-def validate_email(email):
-    """Check email validity"""
-    pattern = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
-    if re.match(pattern, email):
-        return True
-    return False
+    email, authentication_status, username = Authenticator.login('Login', 'main')
 
-def validate_username(username):
-    """Check email validity"""
-    pattern = "^[a-zA-Z0-9]*$"
-    if re.match(pattern, username):
-        return True
-    return False
+    info, info1 = st.columns(2)
 
-def sign_uo():
-    with st.form(key='signup', clear_on_submit=True):
-        st.subheader('Sign up')
-        email = st.text_input('Email', placeholder='Enter your email')
-        username = st.text_input('Username', placeholder='Enter your username')
-        password1 = st.text_input('Password', placeholder='Enter your password', type='password')
-        password2 = st.text_input('Confirm password', placeholder='Confirm your password', type='password')
+    if not authentication_status:
+        sign_up()
 
-        st.form_submit_button('Sign up')
+    if username:
+        if username in usernames:
+            if authentication_status:
+                st.sidebar.subheader(f'Welcome {username}')
+                Authenticator.logout('Log out', 'sidebar')
 
-sign_uo()
+                st.subheader('Home page')
+                st.markdown("""Created with ♥️ by Janke Coetzee""")
+
+            elif authentication_status == False:
+                with info:
+                    st.error('Incorrect username or password')
+            elif authentication_status == None:
+                with info:
+                    st.warning('Sign in')
+        else:
+            with info:
+                st.warning('Username does not exist')
+
+except:
+    st.success('Refresh page')
