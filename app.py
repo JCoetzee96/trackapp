d25026a1163b8c34ffd2c51cd0084ba1e4a798e1
jankecoetzee <jankecoetzee@hotmail.com>
2024-01-16 16:19:20 +0100

    test

--- app.py
+++ app.py
@@ -1,45 +1,52 @@
-import hmac
 import streamlit as st
+import streamlit_authenticator as stauth
+import datetime
+import re
+from deta import Deta
 
-def check_password():
-    """Returns `True` if the user had a correct password."""
+deta_key = "a0hkj1bnvev_7P3cA3n5tmGqFMu4zrqEHKrah9su9hJQ"
+deta = Deta(deta_key)
+db = deta.Base('Authentication')
 
-    def login_form():
-        """Form with widgets to collect user information"""
-        with st.form("Credentials"):
-            st.title("Login")
-            st.text_input("Username", key="username")
-            st.text_input("Password", type="password", key="password")
-            st.form_submit_button("Log in", on_click=password_entered)
+def insert_user(email, username, password):
+    """Insert users to DB"""
+    date_joined = str(datetime.datetime.now())
+    return db.put({'key':email, 'username':username, 'password': password, 'date_joined':date_joined})
 
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["username"] in st.secrets[
-            "passwords"
-        ] and hmac.compare_digest(
-            st.session_state["password"],
-            st.secrets.passwords[st.session_state["username"]],
-        ):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the username or password.
-            del st.session_state["username"]
-        else:
-            st.session_state["password_correct"] = False
+def fetch_users():
+    """Fetch users"""
+    return db.fetch().items
 
-    # Return True if the username + password is validated.
-    if st.session_state.get("password_correct", False):
+def get_user_emails():
+    """Fetch user emails"""
+    return [user['key'] for user in db.fetch().items]
+
+def get_usernames():
+    """"Fetch usernames"""
+    return [user['key'] for user in db.fetch().items]
+
+def validate_email(email):
+    """Check email validity"""
+    pattern = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
+    if re.match(pattern, email):
         return True
-
-    # Show inputs for username + password.
-    login_form()
-    if "password_correct" in st.session_state:
-        st.error("😕 User not known or password incorrect")
     return False
 
+def validate_username(username):
+    """Check email validity"""
+    pattern = "^[a-zA-Z0-9]*$"
+    if re.match(pattern, username):
+        return True
+    return False
 
-if not check_password():
-    st.stop()
+def sign_uo():
+    with st.form(key='signup', clear_on_submit=True):
+        st.subheader('Sign up')
+        email = st.text_input('Email', placeholder='Enter your email')
+        username = st.text_input('Username', placeholder='Enter your username')
+        password1 = st.text_input('Password', placeholder='Enter your password', type='password')
+        password2 = st.text_input('Confirm password', placeholder='Confirm your password', type='password')
 
-# Main Streamlit app starts here
-st.write("Personalised tracking app")
-st.button("Click me")
+        st.form_submit_button('Sign up')
+
+sign_uo()
